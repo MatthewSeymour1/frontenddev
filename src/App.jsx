221050d1f2df9cc26f4642a7247c89419234361b
Matthew Seymour (Student) <N00233944@iadt.ie>
2025-09-18 14:00:16 +0100

    first class finished

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./components/ModuleTitle.jsx"
+
+const App = () => {
+
+    return (
+        <>
+        <h1>Hello from App</h1>
+        <ModuleTitle />
+        </>
+    );
+};
+
+export default App;
---
+++ src/components/ModuleTitle.jsx
@@ -0,0 +1,8 @@
+const ModuleTitle = () => {
+
+    return (
+        <h1>Welcome to the module</h1>
+    );
+};
+
+export default ModuleTitle;
